bf6cd93726a791ad55bd966ab1402ff97d4213a2
kikjs75 <kikjs75@gmail.com>
2026-05-13 20:56:50 +0900

    Header 메뉴 정렬 수정 & Next.js Markdown MDX 시작하기

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from 'next';
+import createMDX from '@next/mdx';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // 마크다운과 MDX 파일을 포함하도록 `pageExtensions` 구성
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  // 필요한 경우 여기에 다른 Next.js 설정을 추가할 수 있습니다
   images: {
     remotePatterns: [
       {
@@ -21,4 +25,9 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  // 필요한 마크다운 플러그인을 여기에 추가하세요
+});
+
+// MDX 설정을 Next.js 설정과 병합
+export default withMDX(nextConfig);
