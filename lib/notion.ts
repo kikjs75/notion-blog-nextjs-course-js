8654d2360c9eb445e20b970e3e8a5c3600557ad0
kikjs75 <kikjs75@gmail.com>
2026-05-13 18:37:37 +0900

    notice.tsx 에서 리턴시 문제 설정

--- lib/notion.ts
+++ lib/notion.ts
@@ -64,7 +64,8 @@ export const getPublishedPosts = async (): Promise<Post[]> => {
             ? ((properties.Author.people[0] as PersonUserObjectResponse)?.name ?? '')
             : '',
         date: properties.Date.type === 'date' ? (properties.Date.date?.start ?? '') : '',
-        modifiedDate: page.last_edited_time,
+        // 한글 주석: Post 타입 필수 필드명이 modifiedData라서 정확히 맞춰줍니다.
+        modifiedData: page.last_edited_time,
         slug:
           properties.Slug.type === 'rich_text'
             ? (properties.Slug.rich_text[0]?.plain_text ?? page.id)
