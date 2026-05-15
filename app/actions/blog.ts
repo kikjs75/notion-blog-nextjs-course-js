58c90043f463cad20b5c8104a37bb6088f9b6b7b
kikjs75 <kikjs75@gmail.com>
2026-05-15 15:36:44 +0900

    Next.js 캐시:unstable_cache()-2-무효화 적용

--- app/actions/blog.ts
+++ app/actions/blog.ts
@@ -1,6 +1,8 @@
 'use server';
 
 import { createPost } from '@/lib/notion';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 import { z } from 'zod';
 
 const postSchema = z.object({
@@ -52,14 +54,12 @@ export async function createPostAction(previousState: PostFormState, formData: F
   try {
     const { title, tag, content } = validatedFields.data;
     await createPost({ title: title, tag: tag, content: content });
-
-    return {
-      message: '블로그 포스트가 성공적으로 생성되었습니다.',
-    };
   } catch (err) {
     return {
       message: '블로그 포스트 생성에 실패했습니다.',
       formData: rawFormData,
     };
   }
+  revalidatePath('/');
+  redirect('/');
 }
