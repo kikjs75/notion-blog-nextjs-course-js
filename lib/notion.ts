9e81ac0bb614a0181edee077a2f324d046f5b213
kikjs75 <kikjs75@gmail.com>
2026-05-15 11:31:59 +0900

    서버 액션-1-서버 컴포넌트

--- lib/notion.ts
+++ lib/notion.ts
@@ -194,3 +194,52 @@ export const getPublishedPosts = async ({
     nextCursor: response.next_cursor,
   };
 };
+
+export interface CreatePostParams {
+  title: string;
+  tag: string;
+  content: string;
+}
+
+export const createPost = async ({ title, tag, content }: CreatePostParams) => {
+  const response = await notion.pages.create({
+    parent: {
+      database_id: process.env.NOTION_DATABASE_ID!,
+    },
+    properties: {
+      Title: {
+        title: [
+          {
+            text: {
+              content: title,
+            },
+          },
+        ],
+      },
+      Description: {
+        rich_text: [
+          {
+            text: {
+              content: content,
+            },
+          },
+        ],
+      },
+      Tags: {
+        multi_select: [{ name: tag }],
+      },
+      Status: {
+        select: {
+          name: 'Published',
+        },
+      },
+      Date: {
+        date: {
+          start: new Date().toISOString(),
+        },
+      },
+    },
+  });
+
+  return response;
+};
