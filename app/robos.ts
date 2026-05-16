c88adbd378ed95bd37df74a3adc48f28c118c69c
kikjs75 <kikjs75@gmail.com>
2026-05-16 14:46:58 +0900

    Metadata Files(wth sitemap, robots)-2

--- app/robos.ts
+++ app/robos.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/api/', '/admin/'],
     },
-    sitemap: 'https://notion-blog-nextjs.vercel.app/sitemap.xml',
+    sitemap: `${process.env.NEXT_PUBLIC_SITE_URL}/sitemap.xml`,
   };
 }
